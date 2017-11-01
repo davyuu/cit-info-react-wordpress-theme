d0cc660a581304a40754bfedd6151e0508a43e2b
David Yu <davidyu_2005@hotmail.com>
2017-10-31 21:25:57 -0400

    Unnecessary

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
     <title>CIT Info</title>
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <?php wp_head(); ?>
-	  <link rel="shortcut icon" href="favicon.ico">
 	</head>
   <body>
     <div id="app"></div>
-  	<script type="text/javascript" src="index.js"></script>
   	<?php wp_footer(); ?>
 	</body>
 </html>
